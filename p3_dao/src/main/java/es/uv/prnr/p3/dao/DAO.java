df0ca8f6f4bbfd6b091e8aa7ae38b33c8b9d19ae
Diego Ruiz <dieruiz4@alumni.uv.es>
2025-04-16 12:46:23 +0200

    Ejercicio 1.2 implementado

--- p3_dao/src/main/java/es/uv/prnr/p3/dao/DAO.java
+++ p3_dao/src/main/java/es/uv/prnr/p3/dao/DAO.java
@@ -2,15 +2,20 @@ package es.uv.prnr.p3.dao;
 
 import java.util.List;
 
-public interface DAO<K,T> {
-	   public T getById(K id );
-	   public List<T> findAll();
-	   public void create(T entity);
-	   public void update(T entity);
-	   public void delete(T entity );
-	   
-	   
-	   //TODO Añadir declaración de métodos deleteByID y findByCriteria
-	  
+public interface DAO<K, T> {
+	public T getById(K id);
 
+	public List<T> findAll();
+
+	public void create(T entity);
+
+	public void update(T entity);
+
+	public void delete(T entity);
+
+	// Declaración añadida en el ejercicio 1.2
+	public boolean deleteById(K id);
+
+	// Declaración añadida en el ejercicio 1.2
+	public List<T> findByCriteria(String jpqlCondition);
 }
--- p3_dao/src/main/java/es/uv/prnr/p3/dao/DAOImpl.java
+++ p3_dao/src/main/java/es/uv/prnr/p3/dao/DAOImpl.java
@@ -7,23 +7,23 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-public abstract class DAOImpl<K,T> implements DAO<K,T>{
-	
+public abstract class DAOImpl<K, T> implements DAO<K, T> {
+
 	protected EntityManager em;
 	protected Class<T> entityClass;
-	
+
 	protected DAOImpl(EntityManager em, Class<T> entityClass) {
 		this.em = em;
 		this.entityClass = entityClass;
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<T> findAll(){
+	public List<T> findAll() {
 		Query q = em.createQuery("from " + this.entityClass.getName());
-		return q.getResultList();	
+		return q.getResultList();
 	}
-	
-	public T getById(K id){
+
+	public T getById(K id) {
 		return em.find(entityClass, id);
 	}
 
@@ -45,15 +45,43 @@ public abstract class DAOImpl<K,T> implements DAO<K,T>{
 		em.getTransaction().commit();
 	}
 
-	// TODO Recibe un identificador de una entidad concreta y lo borra devolviendo true o devuelve false
-	
-	   
-	
+	/**
+	 * Método añadido en el ejercicio 1.2.
+	 * 
+	 * Recibe un identificador de una entidad concreta y
+	 * lo borra devolviendo true o false
+	 */
+	@Override
+	public boolean deleteById(K id) {
+		boolean borrado = false;
+		T entity = em.find(entityClass, id);
 
-	// TODO Recibe una condición para la clá usula WHERE con formato JPQL (criteria)
-	// y retorna las entidades que la cumplen 
-	
-	
-	
+		if (entity != null) {
+			this.em.getTransaction().begin();
+			em.remove(entity);
+			this.em.getTransaction().commit();
+
+			borrado = true;
+		}
+
+		return borrado;
+	}
+
+	/**
+	 * Método añadido en el ejercicio 1.2.
+	 * 
+	 * Recibe una condición para la cláusula WHERE con formato JPQL (criteria) y
+	 * retorna las entidades que la cumplen (por ejemplo, firstName = ‘Remko’
+	 * devolvería todos los empleados con dicho nombre)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findByCriteria(String jpqlCondition) {
+		String queryStr = "SELECT e FROM " + entityClass.getSimpleName() + " e WHERE " + jpqlCondition;
+
+		Query query = em.createQuery(queryStr, entityClass);
+
+		return query.getResultList();
+	}
 
 }
